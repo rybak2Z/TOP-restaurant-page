071d80517ef448689806c67a44d69cbf041b3e46
rybak2Z <rybak2Z.000@gmail.com>
2023-07-28 13:08:05 +0200

    Add script for first load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log("test")
+import load from './fist-load.js';
+load();
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
+    firstLoad: './src/first-load.js',
   },
   devtool: 'inline-source-map',
   devServer: {
