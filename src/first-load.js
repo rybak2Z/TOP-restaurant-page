fd5a4d37b0d10c7bb41558f98f67d2fece242d97
rybak2Z <rybak2Z.000@gmail.com>
2023-07-28 17:09:20 +0200

    Add menu

--- src/first-load.js
+++ src/first-load.js
@@ -13,6 +13,11 @@ function addHeader() {
 }
 
 function addMainContent() {
+  addDescription();
+  addMenu();
+}
+
+function addDescription() {
   let main = document.createElement('main');
   let description = document.createElement('p');
   description.innerText = 'Welcome to Coder\'s Café! Every coder will find ' +
@@ -20,4 +25,24 @@ function addMainContent() {
     'languages is!';
   main.appendChild(description);
   document.body.appendChild(main);
+}
+
+function addMenu() {
+  let menuSection = document.createElement('section');
+  let heading = document.createElement('h2');
+  heading.innerText = 'Menu';
+  menuSection.appendChild(heading);
+  menuSection.appendChild(generateItemsList());
+  document.body.appendChild(menuSection);
+}
+
+function generateItemsList() {
+  let menuItems = ['Java Beans Coffee', 'Crab Plate', 'Snake Meat Stew', 'C-food'];
+  let list = document.createElement('ul');
+  for (const itemName of menuItems) {
+    let menuItem = document.createElement('li');
+    menuItem.innerText = itemName;
+    list.appendChild(menuItem);
+  }
+  return list;
 }
