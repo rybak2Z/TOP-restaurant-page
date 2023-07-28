a3731e2930213c7bc708b7679d3f6e26019ea511
rybak2Z <rybak2Z.000@gmail.com>
2023-07-28 16:43:47 +0200

    Add description

--- src/first-load.js
+++ src/first-load.js
@@ -1,8 +1,23 @@
 export default function load() {
+  addHeader();
+  addMainContent();
+}
+
+function addHeader() {
   let body = document.body;
   let header = document.createElement('header');
   let restaurantName = document.createElement('h1');
   restaurantName.innerText = 'Coder\'s Café';
   header.appendChild(restaurantName);
   body.appendChild(header)
+}
+
+function addMainContent() {
+  let main = document.createElement('main');
+  let description = document.createElement('p');
+  description.innerText = 'Welcome to Coder\'s Café! Every coder will find ' +
+    'something to enjoy here, no matter how bad their taste in programming ' + 
+    'languages is!';
+  main.appendChild(description);
+  document.body.appendChild(main);
 }
