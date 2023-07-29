775a566762c5a001c6291887058c1f84ec0e345a
rybak2Z <rybak2Z.000@gmail.com>
2023-07-29 21:28:17 +0200

    Remove menu heading

--- src/subpages/menu-page.js
+++ src/subpages/menu-page.js
@@ -30,16 +30,9 @@ export function generateMenuPage() {
   let menuPage = document.createElement('main');
   menuPage.id = 'menu-page';
 
-  addHeading(menuPage);
   addItemsList(menuPage);  
 
   return menuPage;
-}
-
-function addHeading(parent) {
-  let heading = document.createElement('h2');
-  heading.innerText = 'Menu';
-  parent.appendChild(heading);
 }
 
 function addItemsList(parent) {
