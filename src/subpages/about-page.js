1c4c598b39f3adfd70238ff33468113d3cfda409
rybak2Z <rybak2Z.000@gmail.com>
2023-07-29 15:01:22 +0200

    Add photos to about page

--- src/subpages/about-page.js
+++ src/subpages/about-page.js
@@ -1,16 +1,38 @@
+import EntrancePhoto from '../../images/entrance.jpeg';
+import InsidePhoto from '../../images/inside.jpeg';
+import OutsidePhoto from '../../images/outside.jpeg';
+
 export function generateAboutPage() {
   let aboutPage = document.createElement('main');
   aboutPage.id = 'about-page';
+  
+  addAboutText(aboutPage);
+  addPhotos(aboutPage);
 
+  return aboutPage;
+}
+
+function addAboutText(parent) {
   let greeting = document.createElement('h2');
   greeting.innerText = 'Welcome to Coder\'s Café!';
-  aboutPage.appendChild(greeting);
+  parent.appendChild(greeting);
 
   let description = document.createElement('p');
   description.innerText = 'Every coder will find something to enjoy here, ' +
     'no matter how bad their taste in programming languages is!';
-  aboutPage.appendChild(description);
+  parent.appendChild(description);
+}
 
-  return aboutPage;
+function addPhotos(parent) {
+  let photos = document.createElement('div');
+  photos.id = 'restaurant-photos';
+
+  for (const photo of [EntrancePhoto, InsidePhoto, OutsidePhoto]) {
+    const image = new Image();
+    image.src = photo;
+    photos.appendChild(image);
+  }
+
+  parent.appendChild(photos);
 }
 
