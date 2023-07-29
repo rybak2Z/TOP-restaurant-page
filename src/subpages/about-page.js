83c8accaf0b0bde11b595ac7481eacd544e9793e
rybak2Z <rybak2Z.000@gmail.com>
2023-07-29 14:46:02 +0200

    Separate greeting from description

--- src/subpages/about-page.js
+++ src/subpages/about-page.js
@@ -1,11 +1,16 @@
 export function generateAboutPage() {
   let aboutPage = document.createElement('main');
   aboutPage.id = 'about-page';
+
+  let greeting = document.createElement('h2');
+  greeting.innerText = 'Welcome to Coder\'s Café!';
+  aboutPage.appendChild(greeting);
+
   let description = document.createElement('p');
-  description.innerText = 'Welcome to Coder\'s Café! Every coder will find ' +
-    'something to enjoy here, no matter how bad their taste in programming ' + 
-    'languages is!';
-  aboutPage.appendChild(description)
+  description.innerText = 'Every coder will find something to enjoy here, ' +
+    'no matter how bad their taste in programming languages is!';
+  aboutPage.appendChild(description);
+
   return aboutPage;
 }
 
