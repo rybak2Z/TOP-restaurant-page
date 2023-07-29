ebb5377d590ebaccc28871d35923ef78284954c0
rybak2Z <rybak2Z.000@gmail.com>
2023-07-29 20:49:28 +0200

    Create menu items list

--- src/subpages/menu-page.js
+++ src/subpages/menu-page.js
@@ -1,3 +1,31 @@
+import CoffeePhoto from '../../images/meals/coffee.jpg';
+import CrabPlatePhoto from '../../images/meals/crab-plate.jpg';
+import MeatStewPhoto from '../../images/meals/meat-stew.jpg';
+import SeafoodPhoto from '../../images/meals/seafood.jpg';
+
+const menuItems = [
+  {
+    name: 'Java Beans Coffee',
+    price: '$3.99',
+    photo: CoffeePhoto,
+  },
+  {
+    name: 'Crab Plate',
+    price: '$24.99',
+    photo: CrabPlatePhoto,
+  },
+  {
+    name: 'Snake Meat Stew',
+    price: '14.99',
+    photo: MeatStewPhoto,
+  },
+  {
+    name: 'C-food Bowl',
+    price: '$19.99',
+    photo: SeafoodPhoto,
+  },
+];
+
 export function generateMenuPage() {
   let menuPage = document.createElement('main');
   menuPage.id = 'menu-page';
